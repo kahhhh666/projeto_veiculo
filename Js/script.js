acb1a94ca5d9fe1e55e124292ba60380272fad60
projeto_veiculo <megaplusms2@gmail.com>
2026-04-12 16:05:36 -0300

    Versão Final, projeto CONCLUIDO. Finalização da pagina Java Script Com o fechamento da etapa final do Codigo, A criação da lista e manipulação dela, a criação de função de exclusão de elementos (veiculos), e lapidação de, tanto do CÓDIGO JavaScript quanto do CÓDIGO html.

--- Js/script.js
+++ Js/script.js
@@ -1,38 +1,85 @@
-// Indentificando elementos do html FORM e div LISTAGEM DE VEICULOS 
-const form = document.querySelector("#formVeiculo")
-const lista = document.querySelector("#listaVeiculos")
+function adicionarVeiculo() {
 
-form.addEventListener("submit", function(e) {
-//faz com que a página não limpe quando eviado
-    e.preventDefault()
-})
+    // Indentificando elementos do html FORM e div LISTAGEM DE VEICULOS 
+    const form = document.querySelector("#formVeiculo")
+    const lista = document.querySelector("#listaVeiculos")
+    const item = document.createElement("div")
+    item.classList.add("item")
 
-//pegando Valores do HTML
 
-const modelo = document.getElementById("modelo").value
-const marca = document.getElementById("marca").value
-const placa = document.getElementById("placa").value
-const ano = document.getElementById("ano").value
-const valor = document.getElementById("valor").value
-const cor = document.getElementById("cor").value
+    //pegando Valores do HTML
 
-//pegando seleção de combustivel
+    const modelo = document.getElementById("modelo").value
+    const marca = document.getElementById("marca").value
+    const placa = document.getElementById("placa").value
+    const ano = parseInt(document.getElementById("ano").value)
+    const valor = parseFloat(document.getElementById("valor").value)
+    const cor = document.getElementById("cor").value
 
-const combustivel = document.querySelector('input[name="combustivel"]:checked')?.value || "Não informado"
+    //pegando seleção de combustivel
 
-//definição das TAXAS dos combustiveis
+    const combustivel = document.querySelector('input[name="combustivel"]:checked')?.value || "Não informado"
 
-const taxas = {
-    gasolina: 0.2,
-    etanol: 0.15,
-    bicombustivel: 0.1,
-    hibrido: 0.08,
-    eletrico: 0.02
+    //definição das TAXAS dos combustiveis
+
+    const taxas = {
+        gasolina: 0.2,
+        etanol: 0.15,
+        bicombustivel: 0.1,
+        hibrido: 0.08,
+        eletrico: 0.02
+    }
+
+
+
+    //calculo taxa de seguro do veiculo
+
+    const seguro = valor * 0.1
+
+    //Final / calculo idade e IPVA
+
+    const anoAtual = new Date().getFullYear()
+    const idade_carro = anoAtual - ano
+
+    let ipva = 0
+    let ipvaTexto = ""
+
+
+    if (idade_carro >= 20) {
+        ipvaTexto = "Isento"
+    } else {
+        const taxa = taxas[combustivel] || 0;
+        ipva = valor * taxa
+        ipvaTexto = `R$ ${ipva.toLocaleString("pt-BR", {style: "currency", currency: "BRL"})}`
+    }
+
+    const total = seguro + ipva
+
+    //criando lista
+
+    item.innerHTML =
+        `
+        <hr>
+
+        <p><strong>Placa:</strong> ${placa}</p>
+        <p><strong>Modelo:</strong> ${modelo}</p>
+        <p><strong>Marca:</strong> ${marca}</p>
+        <p><strong>Ano:</strong> ${ano}</p>
+        <p><strong>Cor:</strong> ${cor}</p>
+        <p><strong>Seguro:</strong> R$ ${seguro.toLocaleString("pt-BR", {style: "currency", currency: "BRL"})}</p>
+        <p><strong>IPVA:</strong> ${ipvaTexto}</p>
+        <p><strong>Total:</strong> R$ ${total.toLocaleString("pt-BR", {style: "currency", currency: "BRL"})}</p>
+
+        <button class="remover">Excluir</button>
+    `
+    const btnExcluir = item.querySelector(".remover");
+
+    btnExcluir.addEventListener("click", function () {
+        if (confirm("Tem certeza que deseja excluir esse veículo?")) {
+            item.remove();
+        }
+    })
+    lista.appendChild(item)
+    //limpa o formulário
+    form.reset()
 }
-
-const taxa = taxas[combustivel] || 0;
-
-//calculo taxa de seguro do veiculo
-
-const seguro = valor * 0.1
-
