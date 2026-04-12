0d4fff8b3d9b1f2110b28969d4bc42a41e89f985
projeto_veiculo <megaplusms2@gmail.com>
2026-04-12 14:34:27 -0300

    Correção do html (adicionado Input Cor veiculo) / Definição da tTaxas dos cmbustiveis ( além da coleta do valor COR do html)

--- Js/script.js
+++ Js/script.js
@@ -14,3 +14,18 @@ const marca = document.getElementById("marca").value
 const placa = document.getElementById("placa").value
 const ano = document.getElementById("ano").value
 const valor = document.getElementById("valor").value
+const cor = document.getElementById("cor").value
+
+//pegando seleção de combustivel
+
+const combustivel = document.querySelector('input[name="combustivel"]:checked')?.value || "Não informado"
+
+//definição das TAXAS dos combustiveis
+
+const taxas = {
+    gasolina: 0.2,
+    etanol: 0.15,
+    bicombustivel: 0.1,
+    hibrido: 0.08,
+    eletrico: 0.02
+}
